a5d48c41c98f653d9a51eea27745ab0baa5e99f6
daniel.aristizabal <daniel.aristizabal@sofka.com.co>
2024-10-03 19:18:14 -0500

    Configuracion inicial agregando modulo de usuarios

---
+++ msvc-usuario/src/test/java/org/daristy/springcloud/msvc/usuarios/MsvcUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daristy.springcloud.msvc.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
