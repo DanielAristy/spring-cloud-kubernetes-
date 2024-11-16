07730f68d6527d90e5275b0f8eb80d518b295297
Daniel Aristizabal <danielaristy22@gmail.com>
2024-11-16 14:19:57 -0500

    Se agrega nuevo modulo de cursos

---
+++ msvc-curso/src/test/java/org/daristy/springcloud/msvc_curso/MsvcCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daristy.springcloud.msvc_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
